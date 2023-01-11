ea45e0bad710f351af54a96ded980e96ea8282e6
Kusum Chowdhury <108144578+kusum-chowdhury@users.noreply.github.com>
2023-01-11 20:38:42 +0000

    done

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require("express");
-
 const { connectDB } = require("./config/db");
 const userRoutes = require("./routes/user");
-
+const productRoutes = require("./routes/product")
 const PORT = 1337;
 
 const app = express();
+
 connectDB();
-
+//middlewares
 app.use(express.json());
-
-express.static("Content");
+app.use(express.static('content'));
+app.use(express.urlencoded({extends: false}));
 
 // Routes
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/product", productRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at port:${PORT}`);
+ 
 });
+
